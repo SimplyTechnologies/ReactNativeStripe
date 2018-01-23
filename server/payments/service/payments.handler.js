261382d902032cba99d928171f06156a588b3e4e
Ani Alaverdyan <anialaverdyan2@gmail.com>
2018-01-23 18:57:08 +0400

    when updating or deleteing the default card, get a new default card

--- server/payments/service/payments.handler.js
+++ server/payments/service/payments.handler.js
@@ -28,7 +28,7 @@ const saveCustomer = (userId, customer) => {
 export const addCard = (req, res, next) => {
   req.user = {
     username: "ani",
-    customerId: "cus_CBQpcvg36VnXh1",
+    customerId: "cus_CBQtywlg6bNV7C",
     _id: "5a65a824e762c8e0226b5a3a"
   };
   const { username, customerId, _id } = req.user;
@@ -90,14 +90,25 @@ export const getCards = (req, res, next) => {
 };
 
 export const deleteCard = (req, res, next) => {
-  req.user = { customerId: "cus_CBQpcvg36VnXh1" };
+  req.user = { customerId: "cus_CBQtywlg6bNV7C" };
   const { customerId } = req.user;
   if (!customerId) {
     return res.status(400).json({ message: "Customer does not exist" });
   }
   const { id } = req.params;
-  stripeHelper
-    .deleteCustomerSource(customerId, id)
+  const deleteCardPromise = stripeHelper.deleteCustomerSource(customerId, id);
+  const retrieveCustomerPromise = deleteCardPromise.then(() =>
+    stripeHelper.retrieveCustomer(customerId)
+  );
+  Promise.all([deleteCardPromise, retrieveCustomerPromise])
+    .then(data => {
+      res.json([
+        data[0],
+        {
+          default_source: data[1].default_source
+        }
+      ]);
+    })
     .then(confirmation => res.json(confirmation))
     .catch(err => next(err.message));
 };
@@ -113,18 +124,27 @@ export const updateCard = (req, res, next) => {
   if (!tokenId) {
     res.status(400).json({ message: "Token is required" });
   }
-  const deletePromise = stripeHelper.deleteCustomerSource(customerId, id);
-  const createPromise = stripeHelper.createCustomerSource(customerId, tokenId);
-  Promise.all([deletePromise, createPromise])
+  const deleteCardPromise = stripeHelper.deleteCustomerSource(customerId, id);
+  const retrieveCustomerPromise = deleteCardPromise.then(() =>
+    stripeHelper.retrieveCustomer(customerId)
+  );
+  const createCardPromise = stripeHelper.createCustomerSource(
+    customerId,
+    tokenId
+  );
+  Promise.all([deleteCardPromise, retrieveCustomerPromise, createCardPromise])
     .then(data => {
       res.json([
         data[0],
         {
-          id: data[1].id,
-          last4: data[1].last4,
-          exp_month: data[1].exp_month,
-          exp_year: data[1].exp_year,
-          brand: data[1].brand
+          default_source: data[1].default_source
+        },
+        {
+          id: data[2].id,
+          last4: data[2].last4,
+          exp_month: data[2].exp_month,
+          exp_year: data[2].exp_year,
+          brand: data[2].brand
         }
       ]);
     })
