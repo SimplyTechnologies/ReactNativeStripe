1da8dc5dae94b2c60c34fba5f4d649366b542a09
tnarek99 <narektumanyan99@gmail.com>
2018-01-23 13:39:08 +0400

    excluded render methods from flowtype errors

--- client/src/screens/constants.js
+++ client/src/screens/constants.js
@@ -1,6 +1,8 @@
 // screen names
 export const CARDS_SCREEN = "app.CardsScreen";
 export const PLANS_SCREEN = "app.PlansScreen";
+export const PAYMENTS_SCREEN = "app.PaymentsScreen";
 // screen labels/titles
 export const CARDS_SCREEN_TITLE = "My Cards";
 export const PLANS_SCREEN_TITLE = "My Plans";
+export const PAYMENTS_SCREEN_TITLE = "Payments";
