5b57388d1ef034ca340746b5f62df6c7eeb80d8c
tnarek99 <narektumanyan99@gmail.com>
2018-01-29 17:44:51 +0400

    Fixed importing issue

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -6,4 +6,3 @@ export { initializeScreens } from "./initializeScreens";
 export { initializeEvents } from "./initializeEvents";
 export { eventHandlers } from "./eventHandlers";
 export { startLoginScreen, startApp } from "./startScreens";
-export { NavigatorEventHandlers } from "./NavigatorEventHandlers";
