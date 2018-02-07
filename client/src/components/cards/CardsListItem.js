0bdf627bb32723870b5065da598be5bc540d6c76
tnarek99 <narektumanyan99@gmail.com>
2018-02-07 15:47:36 +0400

    CardListItem styling

--- client/src/components/cards/CardsListItem.js
+++ client/src/components/cards/CardsListItem.js
@@ -1,6 +1,6 @@
 // @flow
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import type { Card } from "../types";
 
 type Props = {
@@ -16,12 +16,38 @@ export const CardsListItem = ({
   card: { last4, id },
   deleteCardRequest
 }: Props) => (
-  <View>
-    <Text>{last4}</Text>
+  <View style={styles.container}>
+    <Text style={styles.cardNumberText}>...{last4}</Text>
     <TouchableOpacity
+      style={styles.deleteButton}
       onPress={getDeleteButtonPressedHandler(deleteCardRequest, id)}
     >
-      <Text>Delete</Text>
+      <Text style={styles.deleteButtonText}>Delete</Text>
     </TouchableOpacity>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  cardNumberText: {
+    alignItems: "center",
+    fontSize: 15,
+    padding: 10
+  },
+  deleteButton: {
+    alignItems: "center",
+    backgroundColor: "#FC4040",
+    padding: 12,
+    width: 80
+  },
+  deleteButtonText: {
+    color: "white"
+  }
+});
