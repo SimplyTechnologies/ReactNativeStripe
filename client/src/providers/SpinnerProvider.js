d2610a416d616ae9f23cdfba339f5d441fa8a898
tnarek99 <narektumanyan99@gmail.com>
2018-02-08 19:34:18 +0400

    Added SpinnerProvider HOC git add .git add . Was WRONGLY COMMENTED on the previous commit git add .git add .

---
+++ client/src/providers/SpinnerProvider.js
@@ -0,0 +1,49 @@
+// @flow
+import React, { Component } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+
+type Props = {};
+
+type State = {
+  visible: boolean
+};
+
+export const SpinnerProvider = (WrappedComponent: any): any => {
+  class Wrapper extends Component<Props, State> {
+    state = { visible: false };
+
+    show = () =>
+      console.log("CALLING SHOW") || this.setState({ visible: true });
+
+    hide = () =>
+      console.log("CALLING HIDE") || this.setState({ visible: false });
+
+    render() {
+      const { visible } = this.state;
+      return (
+        <View>
+          {visible ? <ActivityIndicator size="large" color="#0000ff" /> : null}
+          <WrappedComponent
+            showSpinner={this.show}
+            hideSpinner={this.hide}
+            {...this.props}
+          />
+        </View>
+      );
+    }
+  }
+
+  return Wrapper;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
