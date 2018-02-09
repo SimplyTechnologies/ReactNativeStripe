6bf95d37f0b506bada154980c5a4eef6814e54d2
tnarek99 <narektumanyan99@gmail.com>
2018-02-09 14:46:02 +0400

    Added plans proxy functions

--- client/src/proxy/index.js
+++ client/src/proxy/index.js
@@ -3,5 +3,11 @@
  */
 
 export { payWithToken, payWithCard, payWithDefaultCard } from "./payment.proxy";
+export {
+  getPlans,
+  getSubscriptions,
+  addSubscription,
+  deleteSubscription
+} from "./plans.proxy";
 export { userLogin, userRegister } from "./user.proxy";
 export { addCard, getCards, deleteCard } from "./card.proxy";
---
+++ client/src/proxy/plans.proxy.js
@@ -0,0 +1,20 @@
+// @flow
+import { fetchUtils } from "AppUtils";
+import { HttpMethods } from "AppConstants";
+
+const EMPTY_OBJECT = {};
+const PLANS_PROXY_URI = "/subscriptions";
+const { makeRequest } = fetchUtils;
+const { GET, POST, DELETE } = HttpMethods;
+
+type Plan = {};
+
+export const getPlans = () => makeRequest(`${PLANS_PROXY_URI}/plans`);
+
+export const getSubscriptions = () => makeRequest(`${PLANS_PROXY_URI}/`);
+
+export const addSubscription = (plan: Plan) =>
+  makeRequest(`${PLANS_PROXY_URI}/plans`, POST, EMPTY_OBJECT, { plan });
+
+export const deleteSubscription = (id: string) =>
+  makeRequest(`${PLANS_PROXY_URI}/${id}`, DELETE);
